d5af7c00cbe009e273cec7adced36812b5a1be3a
sudogie3 <szymon@skierski.com>
2026-01-17 12:46:51 +0100

    added helpers functions and started table function

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,4 +1,5 @@
 import argparse
+import csv
 import wordfreq
 import requests
 from bs4 import BeautifulSoup
@@ -12,25 +13,61 @@ class Scraper():
         self.link = link_URL
         self.use_lokal = use_local_html_file_instead
 
-    def summary(self , phrase):
+    def changingSpaceTo_(self , phrase):
         phrase_tmp = ''
         for charackter in phrase:
             if charackter == ' ':
                 phrase_tmp = phrase_tmp + '_'
             else:
                 phrase_tmp = phrase_tmp + charackter
-        print(phrase_tmp)
-        URL_tmp = self.link + '/'
-        URL_tmp += phrase_tmp
-        print(URL_tmp)
-        response = requests.get(URL_tmp)
-        print(response.status_code)
+        return phrase_tmp
+
+    def SiteDownloader(self, URL):
+        response = requests.get(URL)
         if response.status_code != 200:
-            print(f"Nie ma takiej strony: {URL_tmp}")
+            print(f"Nie ma takiej strony: {URL}")
+            return None
         soup = BeautifulSoup(response.text, 'html.parser')
-        para = soup.find('p').text
+        return soup
+
+    def summary(self , phrase):
+        phrase_tmp = self.changingSpaceTo_(phrase)
+        URL_tmp = self.link + '/' + phrase_tmp
+        soup = self.SiteDownloader(URL_tmp)
+        para  = soup.find('p').text
         print(para)
-    def table(self , phrase , number , first_row_header):
+
+
+    def table(self , phrase , number , first_row_header = False):
+        phrase_tmp = self.changingSpaceTo_(phrase)
+        URL_tmp = self.link + '/' + phrase_tmp
+        soup = self.SiteDownloader(URL_tmp)
+        file_name = f'{phrase_tmp}.csv'
+        tables = soup.find_all('table')
+        if number > len(tables):
+            print(f"Na stronie {URL_tmp} nie ma tylu tabel")
+            return None
+
+        table = tables[number - 1]
+        rows = table.find_all('tr') # wiersze
+        data = []
+        for row in rows:
+            dataAndHeadersInRow = row.find_all(['td' , 'th']) # dane i nagłówki
+            dataAndHeadersInRowText = [data.get_text().strip() for data in dataAndHeadersInRow] # bierzemy jedynie tekst
+                                                                                      # z każdej komorki
+            data.append(dataAndHeadersInRowText)
+        print(data)
+
+        # with open(file_name , 'w' , encoding='utf-8') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     for row in rows:
+        #         writer.writerow(row)
+
+
+
+
+
+
 
         
 
@@ -61,4 +98,4 @@ def creating_parser():
 if __name__ == '__main__':
     URL = 'https://bulbapedia.bulbagarden.net/wiki'
     obiekt = Scraper(URL)
-    obiekt.summary('Team Rocket')
+    obiekt.table('Type' , 2)
